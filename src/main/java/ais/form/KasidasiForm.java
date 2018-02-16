9adbdb9fed4eb2a0196121f0244b21f5326ff381
yuki <yuki@192.168.3.3>
2018-02-16 18:15:20 +0900

    検索機能追加

---
+++ src/main/java/ais/form/KasidasiForm.java
@@ -0,0 +1,5 @@
+package ais.form;
+
+public class KasidasiForm {
+
+}
--- src/main/java/ais/web/KarteKasidasiController.java
+++ src/main/java/ais/web/KarteKasidasiController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ais.dto.KarteKasidasiDto;
+import ais.form.KasidasiForm;
 import ais.util.CsvUtil;
 
 @Controller
@@ -16,13 +18,18 @@ public class KarteKasidasiController {
 
 	@GetMapping
 	public String index(Model model) {
-		List<KarteKasidasiDto>dataList = CsvUtil.importKasidasiCSV("F:\\work\\dev\\workspaces\\samurai_samples\\ais-spring\\src\\main\\resources\\data\\kasidasi.csv");
-		model.addAttribute("datalList",dataList);
+		List<KarteKasidasiDto>dataList = CsvUtil.importKasidasiCSV("C:\\Users\\yuki\\Desktop\\Java関係\\pleiades\\workspace\\ais2\\data\\kasidasi.csv");
+		model.addAttribute("dataList",dataList);
 		return "kasidasi/index" ;
 	}
 
 	@GetMapping(path="search")
 	public String search(Model model) {
 		return "kasidasi/search";
+	}
+
+	@ModelAttribute
+	KasidasiForm setform() {
+		return new KasidasiForm();
 	}
 }
