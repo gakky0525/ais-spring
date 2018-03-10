008a2e95efbe09ac0f63ff64efd912867774ac91
hfujisawa <sportster77749@gmail.com>
2018-03-10 12:42:52 +0900

    誕生日のバリデーション実装

--- src/main/java/ais/web/KarteController.java
+++ src/main/java/ais/web/KarteController.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,7 +48,11 @@ public class KarteController {
 	}
 
 	@PostMapping("registKarte")
-	public String registKarte(KarteForm form, Model model) {
+	public String registKarte(KarteForm form, BindingResult bindingResult, Model model) {
+
+		if(bindingResult.hasErrors()) {
+			return "karte/index";
+		}
 
 		/*
 		 * ①エンティティの作成
